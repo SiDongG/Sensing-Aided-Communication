b3adcdc5190b925a4718eec526ffc4b34b999180
Shez Malik <shez.malik00@gmail.com>
2023-03-23 15:57:49 -0400

    working imu client

--- imu_client_backend/client.py
+++ imu_client_backend/client.py
@@ -3,7 +3,7 @@ import sys
 from IMU import *
 import time
 
-HOST, PORT = "192.168.88.20", 1234
+HOST, PORT = "192.168.88.21", 1234
 data = get_imu_data()
 
 # Create a socket (SOCK_DGRAM means a UDP socket)
@@ -15,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             #convert the list to a string and encode it
             data_str = ",".join(str(val) for val in imu_data)
             data_bytes = str.encode(data_str)
-            sock.sendto(sdata_bytes, (HOST,PORT))
+            sock.sendto(data_bytes, (HOST,PORT))
             
-            print("#### SENDING: {}} ####".format(data_str))
+            print("#### SENDING: {} ####".format(data_str))
             time.sleep(1)   
