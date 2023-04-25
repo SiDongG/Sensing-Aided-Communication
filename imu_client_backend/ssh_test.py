2b1724bd72fe98367cb13687db2343d3a64e760c
shezmalik11 <84518637+shezmalik11@users.noreply.github.com>
2023-04-24 20:25:33 -0400

    adding sector calcs

--- imu_client_backend/ssh_test.py
+++ imu_client_backend/ssh_test.py
@@ -5,8 +5,19 @@ from tx_dict import tx_dict
 router_ip = '192.168.88.11'
 router_username = 'admin'
 beamangle = -7.3
+if beamangle <= 90 or beamangle >= 90:
+    if beamangle > 30:
+        print('sector C')
+        beamangle-= 60
+    elif beamangle < -30:
+        print('sector A')
+        beamangle += 60
+    else:
+        print('sector B')
+        
 # Define the command to change tx sector
 tx_val = tx_dict[beamangle]
+
 print(f'tx_sector:{tx_val}')
 
 router_command = f'/interface w60g> set 0 tx-sector={tx_val}'
@@ -15,5 +26,4 @@ router_command = f'/interface w60g> set 0 tx-sector={tx_val}'
 with Connection(host=router_ip, user=router_username) as conn:
     # Execute the user-defined command on the router and print the output
     result = conn.run(router_command, hide=True)
-    print(result.stdout)
-    
+    print(result.stdout) 
